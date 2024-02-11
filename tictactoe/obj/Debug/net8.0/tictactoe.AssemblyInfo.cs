e38dd03b4a5fc43df3e364da4d22699c1bcca57f
nxhel <nihelmadani200@gmail.com>
2024-02-11 18:48:34 -0500

    resolved duplicate issue

--- tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+548920a678e9084eb78336475c367c54e7fb22c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cdb4d10537f52a56dca2d65e77ca3d256a69045")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
